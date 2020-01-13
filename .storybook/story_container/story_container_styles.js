08cdb87c37fcace048851385a2b00cd1cdb70bb2
Thomas Grivet <thomas.grivet@tutanota.com>
2020-01-13 17:35:37 +0100

    on clean folder build

--- .storybook/story_container/story_container_styles.js
+++
@@ -1,7 +0,0 @@
-export default {
-    container: {
-        minHeight: '100vh',
-        backgroundColor: '#f6f6f6',
-        padding: 50
-    }
-};
--- src/components/card/card_styles.js
+++
@@ -1,9 +0,0 @@
-export const styles = {
-    container: {
-        width: 'fit-content',
-        backgroundColor: '#fff',
-        borderRadius: 5,
-        padding: 20,
-        fontFamily: ['Avenir Next', 'open sans', 'Roboto', 'Arial']
-    }
-};
