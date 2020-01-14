176179fcbdcf2512ef09db1972e605acc4f9f0f7
Thomas Grivet <thomas.grivet@tutanota.com>
2020-01-14 10:34:19 +0100

    Improvements

--- publish.js
+++ publish.js
@@ -2,7 +2,8 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const fs = require('fs');
 const yargs = require('yargs').argv;
-var rl = require('readline-sync');
+const rl = require('readline-sync');
+const ora = require('ora');
 
 const ERROR_PREFIX = '[❌]';
 const SUCCESS_PREFIX = '[✓]';
@@ -12,49 +13,52 @@ const semver = yargs.version || 'patch';
 const cleanInput = ({stdout, stderr}) => ({stdout: stdout.replace(/\n/g, ''), stderr: stderr.replace(/\n/g, '')});
 
 const run = async () => {
+    const checkMasterSpinner = ora('Checking current branch...').start();
     const checkMaster = await exec('git rev-parse --abbrev-ref HEAD').then(cleanInput);
     if (checkMaster.stdout !== 'master') {
-        console.error(ERROR_PREFIX, 'Cannot create version when not on master.');
+        checkMasterSpinner.error('Cannot create version when not on master.');
         process.exit(-1);
     }
+    checkMasterSpinner.success('Current branch is master.');
+    const filesResultSpinner = ora('Checking for modified files...');
     const lsFilesResult = await exec('git ls-files -m');
     if (lsFilesResult.stdout) {
-        console.error(ERROR_PREFIX, 'Cannot create version with modified files. Commit or stash them.');
+        filesResultSpinner.error('Cannot create version with modified files. Commit or stash them.');
         process.exit(-1);
     }
+    filesResultSpinner.success('No modified files found.');
     await exec(`npm version ${semver} --no-git-tag-version`);
-
     const packageFile = fs.readFileSync('package.json');
     const { version: inPackageVersion } = JSON.parse(packageFile);
     let version = null;
     let question = null;
     if (inPackageVersion) {
-        question = 'package.json already contains a version entry, is it correct?'
+        question = '️ℹ package.json already contains a version entry, is it correct?'
     } else {
-        question = 'Did not find any version, please specify one:'
+        question = 'ℹ Did not find any version, please specify one:'
     }
 
+    console.log({ inPackageVersion })
     version = rl.question(question, { defaultInput: inPackageVersion });
 
-    await exec(`git commit -am 'up to version ${version}'`);
-    console.log(`Created new version `);
-
+    await exec(`git commit -am 'Up to version ${version}'.`);
+    console.log(SUCCESS_PREFIX, `Created new version commit.`);
     try {
         await exec('git branch build');
         await exec('git checkout build');
-        console.info("Created branch build");
+        console.info(SUCCESS_PREFIX, "Created & switched to branch build.");
         await exec('git push origin build');
-        console.info("pushed branch build");
+        console.info(SUCCESS_PREFIX, "Pushed current build branch.");
 
     } catch (e) {
-        console.info("Branch build already exists");
-        console.info("Checking out branch and fetching latest version");
+        console.info("ℹ Branch build already exists.");
+        console.info("ℹ Checking out branch and fetching latest version.");
         await exec('git checkout build');
-        const pullExec = await exec('git pull origin build');
-        console.info("pulling branch and fetching latest version");
+        await exec('git pull origin build');
+        console.info("ℹ Pulling remote branch and fetching latest version.");
     }
 
-    console.log(`Merging master to build new version`);
+    console.log(`ℹ Merging master to build new version...`);
     await exec(`git merge master --no-ff -m "Merging master for version ${version}"`);
 
     console.log(`Building new package for version ${version}`);
