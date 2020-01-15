398b6d589c379eeb80c80ca51999815c8295fefb
Thomas Grivet <thomas.grivet@tutanota.com>
2020-01-15 13:52:17 +0100

    Updated index

--- src/index.js
+++ src/index.js
@@ -2,24 +2,14 @@ import { AutoComplete } from './autocomplete/autocomplete';
 import { Button } from './button/button';
 import { Card } from './card/card';
 import { Checkbox } from './checkbox/checkbox';
-import { ClickableTextField } from './clickable_text_field/clickable_text_field';
-import { CompanyPickerField } from './company_picker_field/company_picker_field';
-import { Drawer } from './drawer/drawer';
-import { DrawerButton } from './drawer_button/drawer_button';
-import { DrawerCloseIcon } from './drawer_close_icon/drawer_close_icon';
-import { DrawerLogo } from './drawer_logo/drawer_logo';
-import { ErrorCode } from './error_code/error_code';
 import { List } from './list/list';
 import { ListItem } from './list_item/list_item';
-import { PlacePickerField } from './place_picker_field/place_picker_field';
 import { PopperCard } from './popper_card/popper_card';
 import { PopperCardActions } from './popper_card_actions/popper_card_actions';
 import { PopperCardContent } from './popper_card_content/popper_card_content';
 import { PopperCardTitle } from './popper_card_title/popper_card_title';
 import { ProgressBar } from './progress_bar/progress_bar';
-import { Select } from './select/select';
 import { Slider } from './slider/slider';
-import { Snackbar } from './snackbar/snackbar';
 import { Switch } from './switch/switch';
 import { Tag } from './tag/tag';
 import { TextField } from './text_field/text_field';
@@ -32,24 +22,14 @@ export {
     Button,
     Card,
     Checkbox,
-    ClickableTextField,
-    CompanyPickerField,
-    Drawer,
-    DrawerButton,
-    DrawerCloseIcon,
-    DrawerLogo,
-    ErrorCode,
     List,
     ListItem,
-    PlacePickerField,
     PopperCard,
     PopperCardActions,
     PopperCardContent,
     PopperCardTitle,
     ProgressBar,
-    Select,
     Slider,
-    Snackbar,
     Switch,
     Tag,
     TextField,
