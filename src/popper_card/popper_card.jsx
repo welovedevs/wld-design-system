a286f492df0424c497f5754ce56c4e03b9dbf220
Thomas Grivet <thomas.grivet@tutanota.com>
2020-02-04 15:38:26 +0100

    Trying class order

--- src/popper_card/popper_card.jsx
+++ src/popper_card/popper_card.jsx
@@ -136,6 +136,8 @@ const Content = ({
         [onClickAway]
     );
 
+    console.log({ customClasses });
+
     const content = (
         <div className={classes.wrapper}>
             {!dismissArrow && (
