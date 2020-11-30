f81be50bbd5d989343ee2ef83ed1eb9ccd3fca62
Vincent Cotro <vincentcotro@googlemail.com>
2020-11-30 17:25:27 +0400

    Update card shadow style

--- src/card/card_elevation_spring_props.ts
+++ src/card/card_elevation_spring_props.ts
@@ -6,7 +6,8 @@ export const ELEVATION_SPRING_PROPS: { [key in CardVariant | 'regular']: any } =
             boxShadow: '0 2px 10px 0 rgba(0,0,0,0.1)',
         },
         1: {
-            boxShadow: '0 0 0 1px rgba(0,0,0,.08)',
+            boxShadow:
+                'rgba(0, 0, 0, 0.1) 0px -1px 1px 0px, rgba(0, 0, 0, 0.1) 1px 0px 1px 0px, rgba(0, 0, 0, 0.1) -1px 0px 1px 0px, rgba(0, 0, 0, 0.1) 0px 1px 1px 0px',
         },
         drawer: {
             boxShadow: '0 20px 67px 0 rgba(0,0,0,0.3)',
