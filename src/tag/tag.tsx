186d0157a11472475aa826183d75bacd7095be21
val <valentin@welovedevs.com>
2021-02-23 16:43:23 +0100

    tag done

--- src/tag/tag.tsx
+++ src/tag/tag.tsx
@@ -2,14 +2,13 @@ import React, { CSSProperties, ExoticComponent, useMemo } from 'react';
 
 import cn from 'classnames';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { animated, config, useSpring } from 'react-spring';
 
-import { getComponentColor, getHexFromTheme } from '../styles/utils/styles_utils';
+import { getComponentColor, getHexFromTheme } from '../styles';
 
 import { Typography, TypographyProps } from '../typography/typography';
 
 import { Classes, styles } from './tag_styles';
-import { PaletteColors } from '../styles/palette';
+import { PaletteColors } from '../styles';
 import merge from 'lodash/merge';
 
 const useStyles = makeStyles(styles);
@@ -24,7 +23,7 @@ interface Props {
     customClasses?: Classes;
 }
 export const Tag: React.FC<Props> = ({
-    component: Component = animated.div,
+    component: Component = 'div',
     containerRef,
     className,
     color = 'default',
@@ -43,16 +42,15 @@ export const Tag: React.FC<Props> = ({
     const classes = useStyles({ classes: mergedClasses });
     const hexColor = useMemo(() => getHexFromTheme(theme, color), [theme, color]);
 
-    const springProps = useSpring({
+    const styleProps = {
         color: getComponentColor(true, hexColor),
         boxShadow: `0 ${color ? 5 : 10}px ${color ? 15 : 20}px 0 ${getComponentColor(
             Boolean(hexColor),
             hexColor,
             false,
             '#d6d6d6'
-        )}`,
-        config: config.stiff,
-    });
+        )}`
+    };
     const withColor = color && color !== 'default';
     return (
         <Component
@@ -61,7 +59,7 @@ export const Tag: React.FC<Props> = ({
             style={
                 {
                     ...receivedStyle,
-                    ...springProps,
+                    ...styleProps,
                 } as any
             }
             {...other}
