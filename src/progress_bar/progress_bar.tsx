42e031bef708928dd52fd266fc2323a0bdd2aeec
val <valentin@welovedevs.com>
2021-02-24 13:32:04 +0100

    progress bar animation done

--- src/progress_bar/progress_bar.tsx
+++ src/progress_bar/progress_bar.tsx
@@ -2,12 +2,11 @@ import React, { useMemo } from 'react';
 
 import cn from 'classnames';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { animated, useSpring } from 'react-spring';
+import {motion, useMotionValue, useTransform} from 'framer-motion'
 
-import { getComponentColor, getHexFromTheme } from '../styles';
+import { getComponentColor, getHexFromTheme, PaletteColors } from '../styles';
 
 import { Classes, styles } from './progress_bar_styles';
-import { PaletteColors } from '../styles/palette';
 
 const useStyles = makeStyles(styles);
 
@@ -19,31 +18,25 @@ interface Props {
 }
 
 export const ProgressBar: React.FC<Props> = ({
-    value: progressValue = 0,
-    color = 'primary',
-    className,
-    classes: receivedClasses = {},
-}) => {
+                                                 value: progressValue = 0,
+                                                 color = 'primary',
+                                                 className,
+                                                 classes: receivedClasses = {},
+                                             }) => {
     const classes = useStyles({ classes: receivedClasses });
     const theme = useTheme();
     const hexColor = useMemo(() => getHexFromTheme(theme, color), [theme, color]);
+    const xValue = -100
+    const xMotion = xValue + progressValue
 
-    const { translation } = useSpring({
-        from: {
-            translation: -100,
-        },
-        to: {
-            translation: -100 + progressValue,
-        },
-    });
     return (
         <div className={cn(className, classes.container)}>
-            <animated.div
+            <motion.div
                 className={classes.bar}
-                style={{
-                    color: getComponentColor(true, hexColor, false) as any,
-                    transform: translation.to((value) => `translate3d(${value}%, 0, 0)`),
-                }}
+                style={{color: getComponentColor(true, hexColor, false) as any, }}
+                initial={{x : `${xValue}%`}}
+                animate={{x : `${xMotion}%`}}
+                transition={{type: "tween"}}
             />
         </div>
     );
