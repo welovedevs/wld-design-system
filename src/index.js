cc93271cf2f3048f5377e0596676870a6b342f4b
Vincent Cotro <vincent@welovedevs.com>
2022-05-25 17:43:05 +0400

    Add colors in package

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
-import { AutoComplete } from './autocomplete/autocomplete';
-import { Banner } from './banner/banner';
-import { Button } from './button/button';
-import { Card } from './card/card';
-import { Checkbox } from './checkbox/checkbox';
-import { List } from './list/list';
-import { ListItem } from './list_item/list_item';
-import { PopperCard } from './popper_card/popper_card';
-import { PopperCardActions } from './popper_card_actions/popper_card_actions';
-import { PopperCardContent } from './popper_card_content/popper_card_content';
-import { PopperCardTitle } from './popper_card_title/popper_card_title';
-import { ProgressBar } from './progress_bar/progress_bar';
-import { Slider } from './slider/slider';
-import { Switch } from './switch/switch';
-import { Tag } from './tag/tag';
-import { TextField } from './text_field/text_field';
-import { TextFieldIcon } from './text_field_icon/text_field_icon';
-import { Tooltip } from './tooltip/tooltip';
-import { Typography } from './typography/typography';
+import {AutoComplete} from './autocomplete/autocomplete';
+import {Banner} from './banner/banner';
+import {Button} from './button/button';
+import {Card} from './card/card';
+import {Checkbox} from './checkbox/checkbox';
+import {List} from './list/list';
+import {ListItem} from './list_item/list_item';
+import {PopperCard} from './popper_card/popper_card';
+import {PopperCardActions} from './popper_card_actions/popper_card_actions';
+import {PopperCardContent} from './popper_card_content/popper_card_content';
+import {PopperCardTitle} from './popper_card_title/popper_card_title';
+import {ProgressBar} from './progress_bar/progress_bar';
+import {Slider} from './slider/slider';
+import {Switch} from './switch/switch';
+import {Tag} from './tag/tag';
+import {TextField} from './text_field/text_field';
+import {TextFieldIcon} from './text_field_icon/text_field_icon';
+import {Tooltip} from './tooltip/tooltip';
+import {Typography} from './typography/typography';
 
-import Palette from './styles/palette'
+import {
+    primary,
+    secondary,
+    tertiary,
+    dark,
+    danger,
+    safe,
+    darkblue,
+    warn,
+    orange,
+    light
+} from './styles/palette'
 import style from './styles/tailwind.css';
 
 export {
@@ -41,6 +52,15 @@ export {
     TextFieldIcon,
     Tooltip,
     Typography,
-    Palette,
-    style
+    style,
+    primary,
+    secondary,
+    tertiary,
+    dark,
+    danger,
+    safe,
+    darkblue,
+    warn,
+    orange,
+    light
 };
