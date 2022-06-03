b7e9a69f6e7716528cf3138aef52091898f3ac52
clementdevos <cdevos.0@gmail.com>
2022-06-03 14:56:15 +0200

    chore(switch) migrated to tailwind

--- src/switch/switch_styles.ts
+++ src/switch/switch_styles.ts
@@ -6,6 +6,32 @@ export type Classes = {
     [key in StylesKeys]?: string;
 };
 
+export const baseStyles = {
+    container: 'ds-h-fit ds-relative ds-bg-current ds-rounded-full ds-flex ds-items-center ds-justify-center',
+    disabled: 'ds-cursor-not-allowed',
+    thumbContainer: 'ds-w-full ds-flex ds-items-center ds-absolute ds-transition-all',
+    thumb: ' ds-m-1/2 ds-bg-[#f7f7f7] ds-rounded-full',
+    input:
+        'ds-w-full ds-h-full ds-top-0 ds-bottom-0 ds-right-0 ds-left-0 ds-m-0 ds-z-[2] ds-cursor-inherit ds-absolute ds-opacity-0',
+};
+export const sizeStyles = {
+    size_regular: 'ds-min-h-[30px] ds-w-[80px]',
+    thumb_size_regular: 'ds-w-3 ds-h-3 ',
+    size_small: 'ds-min-h-[20px] ds-w-[50px]',
+    thumb_size_small: 'ds-w-1.5 ds-h-1.5',
+} as const;
+
+export const thumbPositionStyles = {
+    regular: {
+        left: 'ds-left-[2px]',
+        right: 'ds-left-[calc(80px-28px-6px)]',
+    },
+    small: {
+        left: 'ds-left-[2px]',
+        right: 'ds-left-[calc(100%-20px-2px)]',
+    },
+};
+
 export const styles = createStyles({
     container: {
         minHeight: 30,
@@ -54,12 +80,5 @@ export const styles = createStyles({
         margin: 0,
         cursor: 'inherit',
         zIndex: 2,
-    },
-    size_small: {
-        width: 60,
-        '& $thumb': {
-            height: 16,
-            width: 16,
-        },
     },
 });
