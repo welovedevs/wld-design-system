b0c6a7457817ea697614cfa63d246ac9cf1b1368
Vincent Cotro <vincent@welovedevs.com>
2022-08-04 09:54:50 +0200

    Removing react-hoc-sortable

--- src/technologies/selected_technologies/selected_technologies.tsx
+++ src/technologies/selected_technologies/selected_technologies.tsx
@@ -2,7 +2,23 @@ import React, { useCallback, useContext, useMemo } from 'react';
 
 // @ts-ignore
 import cn from 'classnames';
-import { arrayMove, SortableContainer, SortableElement, SortableHandle } from 'react-sortable-hoc';
+
+import {
+    DndContext,
+    closestCenter,
+    KeyboardSensor,
+    PointerSensor,
+    useSensor,
+    useSensors,
+} from '@dnd-kit/core';
+import {
+    arrayMove,
+    SortableContext,
+    sortableKeyboardCoordinates, useSortable,
+    verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+
 
 import last from 'lodash/last';
 import { DevTechnology } from '../technologies/technology';
@@ -11,14 +27,15 @@ import { Card, Slider, Tooltip, Typography } from '../../index';
 import { TrashIcon } from '../../assets/icons/trash';
 import { MoveIcon } from '../../assets/icons/move';
 
-const TechnologyRow = SortableElement(
+const TechnologyRow =
     ({
-        item,
-        onDelete: onRemove,
-        onChange,
-        itemsLength,
-        technologyIndex,
-    }: {
+         id,
+         item,
+         onDelete: onRemove,
+         onChange,
+         itemsLength,
+         technologyIndex,
+     }: {
         id: string;
         item: DevTechnology;
         onDelete: (name: string) => void;
@@ -26,11 +43,23 @@ const TechnologyRow = SortableElement(
         technologyIndex: number;
         itemsLength: number;
     }) => {
-        const { technologies, translations } = useContext(TechnologiesPickerContext);
+        const {
+            attributes,
+            listeners,
+            setNodeRef,
+            transform,
+            transition,
+        } = useSortable({id});
+        const style = {
+            transform: CSS.Transform.toString(transform),
+            transition,
+        };
+
+        const {technologies, translations} = useContext(TechnologiesPickerContext);
 
         const sliderChange = useCallback(
             (e) => {
-                onChange({ ...item, value: Number(e.target.value) });
+                onChange({...item, value: Number(e.target.value)});
             },
             [item, onChange]
         );
@@ -43,22 +72,25 @@ const TechnologyRow = SortableElement(
             return `https://process.filestackapi.com/auto_image/${handle ?? '4A5N89okRPW50jRcmkuM'}`;
         }, [item, technologies]);
 
-        let divider = <div className="ds-bg-dark-50 ds-w-[1px] ds-h-6 ds-mx-2" />;
+        let divider = <div className="ds-bg-dark-50 ds-w-[1px] ds-h-6 ds-mx-2"/>;
         return (
             <div
+                ref={setNodeRef}
                 className={'ds flex ds-flex ds-items-center ds-w-full ds-p-0 ds-my-2 ds-relative ds-z-[1400]'}
-                style={{ zIndex: itemsLength - technologyIndex }}
+                style={{...style, zIndex: itemsLength - technologyIndex}}
             >
-                <DragHandle />
+                <button {...attributes} {...listeners} className="ds-flex" type="button">
+                    <MoveIcon className="ds-w-2.5 ds-h-2.5"/>
+                </button>
                 {divider}
                 <Tooltip title={translations.deleteLabel}>
                     <button className="ds-flex" type="button" onClick={() => onRemove(item.name)}>
-                        <TrashIcon className="ds-fill-danger-500 ds-w-3 ds-h-3" />
+                        <TrashIcon className="ds-fill-danger-500 ds-w-3 ds-h-3"/>
                     </button>
                 </Tooltip>
                 {divider}
                 <Card className="ds-w-5 ds-h-5 !ds-p-1 ds-mx-1">
-                    <img className={'ds-object-contain ds-w-full ds-h-full'} alt={item.name} src={imgUrl} />
+                    <img className={'ds-object-contain ds-w-full ds-h-full'} alt={item.name} src={imgUrl}/>
                 </Card>
                 <div className="ds-flex-1">
                     <Typography color="dark" variant="label">
@@ -82,7 +114,7 @@ const TechnologyRow = SortableElement(
                             min={0}
                             max={100}
                             step={5}
-                            classes={{ container: 'ds-w-12 ds-mr-1' }}
+                            classes={{container: 'ds-w-12 ds-mr-1'}}
                             popperCardProps={{
                                 classes: {
                                     popper: 'ds-z-[1302]',
@@ -94,26 +126,53 @@ const TechnologyRow = SortableElement(
             </div>
         );
     }
-);
 
-const SortableTechnologies = SortableContainer(
-    ({
-        items,
-        onDelete,
-        onItemChange,
-        classes,
-        className,
-        itemsLength,
-    }: Props & {
-        itemsLength: number;
-    }) => {
-        return (
-            <div className={cn(classes?.container, 'ds-pr-2 ds-h-full ds-scrollbar ds-overflow-auto', className)}>
+
+const SortableTechnologies = ({
+                                  items,
+                                  onDelete,
+                                  onItemChange,
+                                  classes,
+                                  className,
+                                  itemsLength,
+                                  onSortEnd
+                              }: Props & {
+    itemsLength: number;
+}) => {
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates,
+        })
+    );
+
+    const handleDragEnd = useCallback((event) => {
+        const {active, over} = event;
+
+        if (active.id !== over.id) {
+            const oldIndex = items.indexOf(items.find(({id}) => id === active.id));
+            const newIndex = items.indexOf(items.find(({id}) => id === over.id));
+            return onSortEnd({oldIndex, newIndex});
+        }
+    }, [items]);
+
+
+    return (
+
+        <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
+        >
+            <SortableContext
+                items={items}
+                strategy={verticalListSortingStrategy}
+            >
                 {items.map((item, index) => (
                     <TechnologyRow
                         key={`selected_technology_row_${item.name}_${index}`}
                         onDelete={onDelete}
-                        id={item.name}
+                        id={item.id}
                         onChange={onItemChange}
                         technologyIndex={index}
                         index={index}
@@ -121,39 +180,37 @@ const SortableTechnologies = SortableContainer(
                         itemsLength={itemsLength}
                     />
                 ))}
-            </div>
-        );
-    }
-);
+            </SortableContext>
+        </DndContext>
 
-const DragHandle = SortableHandle(() => (
-    <button className="ds-flex" type="button">
-        <MoveIcon className="ds-w-2.5 ds-h-2.5" />
-    </button>
-));
+    );
+}
+
 
 interface Props {
-    items: Array<DevTechnology>;
+    items: Array<DevTechnology & { id: string }>;
     onChange: (newArrayValue: Array<DevTechnology>) => void;
     onDelete: (id: string) => void;
     className?: string;
     onItemChange: (technology: DevTechnology) => void;
     classes?: { container?: string };
+    onSortEnd: (props: { newIndex: any; oldIndex: any; }) => any
 }
+
 export const SelectedTechnologies: React.FC<Props> = ({
-    items,
-    onChange,
-    onDelete,
-    className,
-    onItemChange,
-    classes = {},
-}) => {
+                                                          items,
+                                                          onChange,
+                                                          onDelete,
+                                                          className,
+                                                          onItemChange,
+                                                          classes = {},
+                                                      }) => {
     const itemsLength = useMemo(() => items.length, [items]);
 
     const move = useCallback(
-        ({ oldIndex, newIndex }) => {
+        ({oldIndex, newIndex}) => {
             if (typeof onChange === 'function') {
-                onChange(arrayMove(items, oldIndex, newIndex).map((data, index) => ({ ...data, index })));
+                onChange(arrayMove(items, oldIndex, newIndex).map((data, index) => ({...data, index})));
             }
         },
         [items, onChange]
