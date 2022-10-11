53e1502adecaf5d23cd53b338011998458108f7d
clementdevos <cdevos.0@gmail.com>
2022-10-11 10:50:18 +0200

    feat(types) small textfields types touchups

--- src/text_field/text_field.tsx
+++ src/text_field/text_field.tsx
@@ -1,4 +1,4 @@
-import React, { ExoticComponent, ReactChildren, useState } from 'react';
+import React, { ExoticComponent, forwardRef, PropsWithChildren, ReactNode, useState } from 'react';
 
 import cn from 'classnames';
 
@@ -15,7 +15,7 @@ interface CustomProps {
     fullWidth?: boolean;
     inputRef?: any;
     containerRef?: any;
-    beforeChildren?: ReactChildren;
+    beforeChildren?: ReactNode;
     multiline?: boolean;
     rows?: number;
     variant?: TextFieldVariants;
@@ -33,79 +33,84 @@ interface CustomProps {
 
 export type TextFieldProps = Omit<React.InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement>, 'size'> &
     CustomProps;
-export const TextField: React.FC<TextFieldProps> = ({
-    containerElement: ContainerElement = 'div',
-    containerProps,
-    className,
-    inputClassName,
-    fullWidth,
-    inputRef,
-    containerRef,
-    beforeChildren = null,
-    multiline,
-    rows,
-    children,
-    variant = 'raised',
-    type = 'text',
-    disabled,
-    size,
-    classes = {},
-    ...other
-}) => {
-    const InputComponent = multiline ? 'textarea' : 'input';
-    const isPassword = type === 'password';
+export const TextField = forwardRef<HTMLElement, PropsWithChildren<TextFieldProps>>(
+    (
+        {
+            containerElement: ContainerElement = 'div',
+            containerProps,
+            className,
+            inputClassName,
+            fullWidth,
+            inputRef,
+            containerRef,
+            beforeChildren = null,
+            multiline,
+            rows,
+            children,
+            variant = 'raised',
+            type = 'text',
+            disabled,
+            size,
+            classes = {},
+            ...other
+        },
+        ref
+    ) => {
+        const InputComponent = multiline ? 'textarea' : 'input';
+        const isPassword = type === 'password';
 
-    const [showHidePassword, changeShowHidePassword] = useState(false);
-    const togglePasswordVisiblity = () => {
-        changeShowHidePassword(!showHidePassword);
-    };
+        const [showHidePassword, changeShowHidePassword] = useState(false);
+        const togglePasswordVisiblity = () => {
+            changeShowHidePassword(!showHidePassword);
+        };
 
-    return (
-        <ContainerElement
-            ref={containerRef}
-            className={cn(
-                className,
-                baseStyles.container,
-                fullWidth && 'w-full',
-                multiline && baseStyles.multilineContainer,
-                variant && variantStyles[variant],
-                disabled && variant && variantStyles[`${variant}Disabled`],
-                classes?.container
-            )}
-            {...(containerProps &&
-                containerProps.style && {
-                    style: containerProps.style,
-                })}
-            {...containerProps}
-        >
-            {beforeChildren}
-            <InputComponent
-                ref={inputRef}
+        return (
+            <ContainerElement
+                ref={ref || containerRef}
                 className={cn(
-                    inputClassName,
-                    baseStyles.input,
-                    multiline && baseStyles.multilineInput,
-                    size && sizeStyles[size],
-                    variant && inputStyles[variant],
-                    disabled && inputStyles.disabled,
-                    disabled && variant && inputStyles[`${variant}Disabled`],
-                    classes?.input
+                    className,
+                    baseStyles.container,
+                    fullWidth && 'w-full',
+                    multiline && baseStyles.multilineContainer,
+                    variant && variantStyles[variant],
+                    disabled && variant && variantStyles[`${variant}Disabled`],
+                    classes?.container
                 )}
-                type={showHidePassword ? 'text' : type}
-                {...{ rows, disabled }}
-                {...other}
-            />
-            {isPassword && (
-                <IconButton
-                    title="Show/Hide password"
-                    className="ds-w-5 ds-h-5 ds-ml-1"
-                    onClick={togglePasswordVisiblity}
-                    size="large"
-                >
-                    {showHidePassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
-                </IconButton>
-            )}
-            {children}
-        </ContainerElement>
-    );
-};
+                {...(containerProps &&
+                    containerProps.style && {
+                        style: containerProps.style,
+                    })}
+                {...containerProps}
+            >
+                {beforeChildren}
+                <InputComponent
+                    ref={inputRef}
+                    className={cn(
+                        inputClassName,
+                        baseStyles.input,
+                        multiline && baseStyles.multilineInput,
+                        size && sizeStyles[size],
+                        variant && inputStyles[variant],
+                        disabled && inputStyles.disabled,
+                        disabled && variant && inputStyles[`${variant}Disabled`],
+                        classes?.input
+                    )}
+                    type={showHidePassword ? 'text' : type}
+                    {...{ rows, disabled }}
+                    {...other}
+                />
+                {isPassword && (
+                    <IconButton
+                        title="Show/Hide password"
+                        className="ds-w-5 ds-h-5 ds-ml-1"
+                        onClick={togglePasswordVisiblity}
+                        size="large"
+                    >
+                        {showHidePassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                    </IconButton>
+                )}
+                {children}
+            </ContainerElement>
+        );
+    }
+);
